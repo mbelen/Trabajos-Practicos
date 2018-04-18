cf8eec305239685424acba1713857a7991daeb12
agusgarcia17 <agus.garcia17@gmail.com>
2018-04-18 15:55:13 -0300

    arreglos

--- TP Uno/js/index.js
+++ TP Uno/js/index.js
@@ -1,16 +1,8 @@
-var slide0 = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("img-slide");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slide0++;
-    if (slide0 > x.length) 
-        {slide0 = 1} 
-    x[slide0-1].style.display = "flex";
-    
-    setTimeout(carousel, 2000)
-}
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      mode: "fade",
+      captions: true,
+  
+      });
+  });
+  
