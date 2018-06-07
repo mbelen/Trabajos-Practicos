8443ecd868100bc38528c839f107651fe8c10a49
agusgarcia17 <agus.garcia17@gmail.com>
2018-06-07 16:00:10 -0300

    tp3

---
+++ TP Tres/js/ranking.js
@@ -0,0 +1,14 @@
+var contenidoLocalStorageJugadas=JSON.parse(localStorage.getItem('jugadas')).jugadas;
+
+function sortAsc(a, b){
+	var aIntentos = a.intentosUsados;
+	var bIntentos = b.intentosUsados;
+  	return ((aIntentos < bIntentos) ? -1 : ((aIntentos > bIntentos) ? 1 : 0));
+}
+
+contenidoLocalStorageJugadas.sort(sortAsc);
+
+//Se genera una fila de tres celdas por cada jugada guardada a mostrar en la tabla
+$.each( contenidoLocalStorageJugadas, function( i, e ){
+    $('#listado').append(`<tr><td>${e.nombre}</td><td>${e.complejidad}</td><td>${e.intentosUsados}</td></tr>}`);
+});
